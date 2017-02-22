742eaf1b05e5fba78159fca2d3bfd0e8fa054168
James Jackson-South <james_south@hotmail.com>
2017-02-23 09:48:59 +1100

    Add unit tests

---
+++ tests/Zoombraco.Tests/EnumExtensionTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Zoombraco.Tests
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Xunit;
+    using Zoombraco.Extensions;
+
+    public class EnumExtensionTests
+    {
+        public static readonly TheoryData<TestEnum, string> EnumData = new TheoryData<TestEnum, string>
+        {
+            {TestEnum.Value1,"I am the first value." },
+            {TestEnum.Value2,"I am the second value." },
+            {TestEnum.Value3,"Value3" },
+        };
+
+        [Theory]
+        [MemberData(nameof(EnumData))]
+        public void EnumReturnsCorrectDisplayValue(TestEnum value, string expected)
+        {
+            Assert.Equal(value.ToDisplay(), expected, StringComparer.OrdinalIgnoreCase);
+        }
+
+    }
+
+    public enum TestEnum
+    {
+        [Display(Name = "I am the first value.")]
+        Value1,
+
+        [Display(Name = "I am the second value.")]
+        Value2,
+
+        Value3
+    }
+}
---
+++ tests/Zoombraco.Tests/StringExtensionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Zoombraco.Tests
+{
+    using Xunit;
+    using Zoombraco.Extensions;
+
+    public class StringExtensionTests
+    {
+        public static readonly TheoryData<string, bool> UrlData = new TheoryData<string, bool>
+        {
+            {"http://umbraco.com", true },
+            {"https://umbraco.com", true },
+            {"https://umbraco.com/test.html?w=10", true },
+            {"ftp://umbraco.com/test.html?w=10", true },
+            {"umbraco.com", false },
+            {"/test.html", false },
+            {"/test.html?w=10", false },
+        };
+
+        [Theory]
+        [MemberData(nameof(UrlData))]
+        public void StringCanCorrectlyDetermineAbsoluteUrl(string url, bool expected)
+        {
+            Assert.Equal(url.IsAbsoluteUrl(), expected);
+        }
+    }
+}

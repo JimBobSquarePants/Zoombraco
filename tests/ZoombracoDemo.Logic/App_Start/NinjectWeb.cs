0f8f1494c18ce12e0912bb99e14bce7c4fffdb8e
James Jackson-South <james_south@hotmail.com>
2017-07-25 08:29:53 +1000

    Cleanup

--- tests/ZoombracoDemo.Logic/App_Start/NinjectWeb.cs
+++ tests/ZoombracoDemo.Logic/App_Start/NinjectWeb.cs
@@ -1,5 +1,10 @@
-﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(ZoombracoDemo.Logic.NinjectWeb), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(ZoombracoDemo.Logic.NinjectWeb), "Stop")]
+﻿// <copyright file="NinjectWeb.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(ZoombracoDemo.Logic.NinjectWeb), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(ZoombracoDemo.Logic.NinjectWeb), "Stop")]
 
 namespace ZoombracoDemo.Logic
 {
--- tests/ZoombracoDemo.Logic/App_Start/WebApiConfig.cs
+++ tests/ZoombracoDemo.Logic/App_Start/WebApiConfig.cs
@@ -1,9 +1,21 @@
-﻿namespace ZoombracoDemo.Logic
+﻿// <copyright file="WebApiConfig.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ZoombracoDemo.Logic
 {
     using System.Web.Http;
 
+    /// <summary>
+    /// Provides configuration for the Web.Api routes
+    /// </summary>
     public static class WebApiConfig
     {
+        /// <summary>
+        /// Registers the configuration
+        /// </summary>
+        /// <param name="config">The HttpServer configuration</param>
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -14,8 +26,7 @@
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                defaults: new { id = RouteParameter.Optional });
         }
     }
 }
